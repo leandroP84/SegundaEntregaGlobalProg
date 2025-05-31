de749b8503ce9c1942e6ef88e49393fa5ed262b4
Franco Castro <arcefr34@gmail.com>
2025-05-30 23:26:01 -0300

    Cambio en clase Articulo y Categoria

--- Proyecto2Prog(Final-Lombok-Update)2/Proyecto2Prog/Proyecto2Prog/src/main/java/org/example/Articulo.java
+++ Proyecto2Prog(Final-Lombok-Update)2/Proyecto2Prog/Proyecto2Prog/src/main/java/org/example/Articulo.java
@@ -8,18 +8,18 @@ import lombok.experimental.SuperBuilder;
 @Setter
 @ToString
 @SuperBuilder
-
-public class Articulo extends Base {
+public abstract class Articulo extends Base {
     protected String denominacion;
     protected Double precioVenta;
     protected UnidadMedida unidadMedida;
 
     protected ArrayList<Imagen> imagenes = new ArrayList<>();
 
-    public void addUnidadMedida(UnidadMedida unidadMedida){
+    public void addUnidadMedida(UnidadMedida unidadMedida) {
         this.unidadMedida = unidadMedida;
     }
-    public void addImagen(Imagen imagen){
+
+    public void addImagen(Imagen imagen) {
         this.imagenes.add(imagen);
     }
 }
--- Proyecto2Prog(Final-Lombok-Update)2/Proyecto2Prog/Proyecto2Prog/src/main/java/org/example/Categoria.java
+++ Proyecto2Prog(Final-Lombok-Update)2/Proyecto2Prog/Proyecto2Prog/src/main/java/org/example/Categoria.java
@@ -2,20 +2,28 @@ package org.example;
 
 import java.util.ArrayList;
 
-import lombok.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 import lombok.experimental.SuperBuilder;
-
+import org.example.Articulo;
+@SuperBuilder
 @Getter
 @Setter
 @ToString
-@SuperBuilder
 
-public class Categoria extends Base {
-    private  String denominacion;
-    private ArrayList<Categoria> subCategorias;
-    private ArrayList<Articulo> articulos = new ArrayList<>(); //⬜
+public class Categoria {
+    private String denominacion;
+    private ArrayList<Categoria> subCategorias = new ArrayList<>();
+    private ArrayList<Articulo> articulos = new ArrayList<>();
 
-    public void addArticulo(Articulo articulo){
-        this.articulos.add(articulo);
+
+    public void addSubCategoria(Categoria categoria) {
+        this.subCategorias.add(categoria);
     }
+
+
+
+    public void addArticulo(Articulo articulo) {
+        this.articulos.add(articulo);}
 }
